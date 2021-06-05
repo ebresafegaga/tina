0ec5e0cbff6d577b398f53bcc664197ff1364deb
ebresafegaga <ebresafegaga@outlook.com>
2021-06-05 18:13:32 +0100

    Pretty printing for values

--- runtime/value.ml
+++ runtime/value.ml
@@ -11,5 +11,19 @@ type value =
     | VClosure of (value list -> (value, string) result)
     | VRecord of DataName.t * (FieldName.t * value) list 
 
-let pp_value ppf v = 
-    failwith ""
+let rec pp_value v = 
+    match v with 
+    | VUnit -> "(void)"
+    | VInteger i -> Int.to_string i
+    | VString s -> Printf.sprintf "%s%s%s" {|"|} s {|"|} 
+    | VFloat f -> Float.to_string f 
+    | VBool b -> Bool.to_string b
+    | VClosure clo -> "<fun>"
+    | VRecord (name, fields) ->
+        let pp_fields = 
+            fields  
+            |> List.map (fun (name, value) -> 
+                Printf.sprintf " %s: %s" (FieldName.to_string name) (pp_value value))
+            |> String.concat ","
+        in
+        Printf.sprintf "%s {%s }" (DataName.to_string name) pp_fields 
